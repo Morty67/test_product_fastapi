69ad7fc5bb575ded8582ade82ac757d31fd0d3a1
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-02 17:27:48 +0300

    "Added schemas.py"

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+from typing import Optional
+from datetime import datetime
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: float
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
+    created_at: Optional[datetime] = datetime.now()
+
+    class Config:
+        orm_mode = True
