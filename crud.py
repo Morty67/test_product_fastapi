2c18221d55f95af6cb3d813ecb0aebc03f75e687
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-02 17:59:52 +0300

    "Added crud.py"

---
+++ crud.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+
+from db.models import Product
+from schemas import ProductCreate
+
+
+def get_all_products(db: Session):
+    return db.query(Product).all()
+
+
+def get_product_by_id(db: Session, product_id: int):
+    return db.query(Product).filter(Product.id == product_id).first()
+
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(
+        name=product.name,
+        description=product.description,
+        price=product.price,
+        )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def update_product(db: Session, product_id: int, product: ProductCreate):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    db_product.name = product.name
+    db_product.description = product.description
+    db_product.price = product.price
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def delete_product(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    db.delete(db_product)
+    db.commit()
+    return db_product
