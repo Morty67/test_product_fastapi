48115305a349ba758f1f5adf9e14b1b38e8ff529
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-06 13:20:41 +0300

    "Added Category model"

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+
+from sqlalchemy.orm import relationship
+
 from app.database.engine import Base
-from sqlalchemy import Column, String, Integer, DateTime, Float
+from sqlalchemy import Column, String, Integer, DateTime, Float, ForeignKey
 
 
 class Product(Base):
@@ -10,8 +13,21 @@ class Product(Base):
     description = Column(String(255))
     price = Column(Float)
     quantity = Column(Integer)
-    category = Column(String(255))
     created_at = Column(DateTime, default=datetime.now())
+
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="products")
 
     def __repr__(self):
         return f"<Product {self.name} - {self.description}>"
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
+
+    products = relationship("Product", back_populates="category")
+
+    def __repr__(self):
+        return f"<Category {self.name}>"
