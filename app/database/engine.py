98cf197ebab5b1762891f3dac84ff8c6985586d4
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-06 14:07:15 +0300

    "Added async_engine"

--- app/database/engine.py
+++ app/database/engine.py
@@ -1,15 +1,13 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+SQLALCHEMY_DATABASE_URI = "sqlite+aiosqlite:///./products.db"
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///./products.db'
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URI,
-    connect_args={'check_same_thread': False},
+engine = create_async_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False, autocommit=False
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
