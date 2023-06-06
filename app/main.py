e3c573ab8b4f2f68f8242ad90067a7dc26cc1d81
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-06-06 13:22:45 +0300

    "Added endpoints for categories"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
+from app.models.models import Category
 from app.services.product_service import ProductService, ProductValidator
 from app.database.engine import SessionLocal
 from app.serializers import schemas
@@ -22,8 +25,16 @@ def get_db() -> Session:
 
 
 @app.get("/products/", response_model=list[schemas.Product])
-def read_all_products(db: Session = Depends(get_db)):
-    return product_service.get_all_products(db=db)
+def read_all_products(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+):
+    offset = (page - 1) * per_page
+    products = product_service.get_all_products(
+        db=db, offset=offset, limit=per_page
+    )
+    return products
 
 
 @app.get("/products/{product_id}", response_model=schemas.Product)
@@ -63,3 +74,55 @@ def delete_product(product_id: int, db: Session = Depends(get_db)):
     if not product_service.delete_product(db=db, product_id=product_id):
         raise HTTPException(status_code=404, detail="Product not found")
     return {"message": "Product deleted"}
+
+
+@app.post("/categories", response_model=schemas.Category)
+def create_category(
+    category: schemas.CategoryCreate, db: Session = Depends(get_db)
+):
+    db_category = Category(name=category.name)
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+
+@app.get("/categories", response_model=List[schemas.Category])
+def get_all_categories(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
+    categories = db.query(Category).offset(skip).limit(limit).all()
+    return categories
+
+
+@app.get("/categories/{category_id}", response_model=schemas.Category)
+def get_category(category_id: int, db: Session = Depends(get_db)):
+    category = db.query(Category).filter(Category.id == category_id).first()
+    if not category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return category
+
+
+@app.put("/categories/{category_id}", response_model=schemas.Category)
+def update_category(
+    category_id: int,
+    category: schemas.CategoryUpdate,
+    db: Session = Depends(get_db),
+):
+    db_category = db.query(Category).filter(Category.id == category_id).first()
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    db_category.name = category.name
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+
+@app.delete("/categories/{category_id}")
+def delete_category(category_id: int, db: Session = Depends(get_db)):
+    db_category = db.query(Category).filter(Category.id == category_id).first()
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    db.delete(db_category)
+    db.commit()
+    return {"message": "Category deleted successfully"}
